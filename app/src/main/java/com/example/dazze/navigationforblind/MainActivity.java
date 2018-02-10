a626fcb852dc299ed5cd806f000f7bce1f3e64d3
tlaektmf <dazzel0826@gmail.com>
2018-02-10 20:50:51 +0900

    보호자 로그인 회원가입
    버튼생성

--- app/src/main/java/com/example/dazze/navigationforblind/MainActivity.java
+++ app/src/main/java/com/example/dazze/navigationforblind/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.dazze.navigationforblind;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPJoin;//보호자 회원가입
+    private Button btnPLogin;//보호자 로그인
+    private Button btnUJoin;
+    private Button btnULogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //드디어 되나여
+
+        /*
+        1. 보호자 회원가입
+        2. 보호자 로그인
+         */
+
+        btnPJoin=(Button)findViewById(R.id.PJoin);
+        btnPLogin=(Button)findViewById(R.id.PLogin);
+
+
+        //
+
+        btnPLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //데이터 확인
+                Intent intent=new Intent(getBaseContext(),NPLoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnPJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //데이터 확인
+                Intent intent=new Intent(getBaseContext(),NPJoinActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/dazze/navigationforblind/NPJoinActivity.java
@@ -0,0 +1,13 @@
+package com.example.dazze.navigationforblind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NPJoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_npjoin);
+    }
+}
