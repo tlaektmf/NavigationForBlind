3ecb5f9d07b83e144dace5bfbdfacf0f82ab81b8
tlaektmf <dazzel0826@gmail.com>
2018-03-01 02:24:24 +0900

    경로를 polyline으로 보여주기
    firebase에서 경로데이터를 받아서
    polyline의 형태로 mapview에 뿌려줌

---
+++ app/src/main/java/com/example/dazze/navigationforblind/NMakeWayActivity.java
@@ -0,0 +1,13 @@
+package com.example.dazze.navigationforblind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NMakeWayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nmake_way2);
+    }
+}
--- app/src/main/java/com/example/dazze/navigationforblind/NMapViewActivity.java
+++ app/src/main/java/com/example/dazze/navigationforblind/NMapViewActivity.java
@@ -1,13 +1,33 @@
 package com.example.dazze.navigationforblind;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPolyline;
 import net.daum.mf.map.api.MapView;
 
-public class NMapViewActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class NMapViewActivity extends AppCompatActivity  implements MapView.OpenAPIKeyAuthenticationResultListener, MapView.MapViewEventListener {
+
+
+    FirebaseDatabase database;
+    DatabaseReference databaseRef;
+    private FirebaseAuth mAuth;
+    private ArrayList mMapPointList;//보호자가 찍은 점들을 리스트로 관리
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +35,104 @@ public class NMapViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nmap_view);
 
         //xml에 선언된 map_view 레이아웃을 찾아온 후, 생성한 MapView객체 추가
-        MapView mapView = new MapView(this);
+        final MapView mapView = new MapView(this);
         mapView.setDaumMapApiKey("63cfe0149022d302d1f2ce09919b176c");
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+        //mapViewContainer.addView(mapView);
 
         /*
-        1. 보호자가 사용자에게 넘긴 경로의 번호 & 개수를 받는다
-        2. 점의 좌표 (위도, 경도) 를 받아
-        3. 점을 찍어주고 이어준다
+        화면에 경로를 보여주기만 하고 다른 기능은 막아둠
+        시각장애인 입장에서 보면, "출발지-도착지 에 해당되는 경로입니다" 부분에 들어가는 activity에 속함
+        즉 이 activity들은 모두 tts로 대체하여 시각장애인에게 제공함
+
+        1. 점의 좌표 (위도, 경도) 를 받아
+        2. 점을 찍어주고 이어준다
          */
+        database = FirebaseDatabase.getInstance();
+        databaseRef = database.getReference("userData");
+        mAuth = FirebaseAuth.getInstance();
+
+        mMapPointList=new ArrayList();
+
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                NWayInfoData data=new NWayInfoData();
+                data = dataSnapshot.child(mAuth.getCurrentUser().getUid().toString()).child("공유데이터").child("m_way").child("0").getValue(NWayInfoData.class);
+
+                for(int i=0;i<data.m_XYList.size();i++){
+                    mMapPointList.add(MapPoint.mapPointWithGeoCoord(Double.parseDouble(data.m_XYList.get(i).m_latitude), Double.parseDouble(data.m_XYList.get(i).m_longitude)));
+                }
 
 
+                //polyline 만듦
+                Log.i("다슬로그",mMapPointList.get(0).toString());
+                MapPolyline polyline = new MapPolyline(mMapPointList.size());
+                polyline.setTag(2000);
+                polyline.setLineColor(Color.argb(128, 255, 93, 117));
+                for(int i=0;i<mMapPointList.size();i++){
+                        polyline.addPoint((MapPoint) mMapPointList.get(i));
+                }
+                mapView.addPolyline(polyline);
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("TAG: ", "Failed to read value", databaseError.toException());
+            }
+        });
+
+        mapViewContainer.addView(mapView);
+
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onDaumMapOpenAPIKeyAuthenticationResult(MapView mapView, int i, String s) {
 
     }
 }
